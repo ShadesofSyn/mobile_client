b4d1ab0f67d5454fc837897d1232bbe70c967b4d
Max Wischusen <maxwischusen@Maxs-MacBook-Pro.local>
2023-12-03 17:16:08 -0500

    changed map to 5500

--- main/world/world/world.gd
+++ main/world/world/world.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var wall_tiles: TileMap = $map/walls
 @onready var boundary_area: CollisionPolygon2D = $boundary/CollisionPolygon2D
 
-@export var hex_size: float = 4000
+@export var hex_size: float = 5500
 @export var spawn_area_radius: float = 640
 @export var redraw: bool : set = redraw_map
 
@@ -76,6 +76,7 @@ func set_lines() -> void:
 	for i in range(3):
 		get_node("lines/quad_lines/"+str(i+1)).points = [Vector2(0,0),Vector2(0,hex_size/pow(3, 1/2.0))]
 	print(hex_size/pow(3, 1/2.0))
+	print(hex_size/pow(3, 1/2.0))
 	get_node("objectives/objectives/1").position = Vector2(-hex_size/3.46,hex_size/2)
 	get_node("objectives/objectives/2").position = Vector2(hex_size/3.46,hex_size/2)
 	get_node("objectives/objectives/3").position = Vector2(-hex_size/3.46,-hex_size/2)
@@ -88,7 +89,7 @@ func init_tall_grass() -> void:
 		var grass = preload("res://main/world/map objects/obstacles/tall_grass/tall_grass.tscn").instantiate()
 		grass.position = loc*80 + Vector2i(40,40)
 		$obstacles.add_child(grass)
-		valid_tiles.set_cell(0,loc,0,Vector2i(1,0))
+#		valid_tiles.set_cell(0,loc,0,Vector2i(1,0))
 	tall_grass_tiles.hide()
 
 
